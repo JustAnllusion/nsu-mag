814cfd8d32e5d909852a15d343790d77903bd769
Дмитрий Цыбульский <dima@Mac-mini-Dmitrij.local>
2022-10-25 23:42:09 +0700

    1

--- py_env/bin/activate.fish
+++ py_env/bin/activate.fish
@@ -30,7 +30,7 @@ end
 # Unset irrelevant variables.
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV "/Users/dtsybulskii/nsu_mag/nsu-mag/py_env"
+set -gx VIRTUAL_ENV "/Users/dima/nsu-mag/py_env"
 
 set -gx _OLD_VIRTUAL_PATH $PATH
 set -gx PATH "$VIRTUAL_ENV/bin" $PATH
